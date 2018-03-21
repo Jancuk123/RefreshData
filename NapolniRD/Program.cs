0fc0f285578733e2b1471375bde87c39deb0b65b
Jan Čuk <jan.cuk@hotmail.com>
2018-03-21 15:08:15 +0100

    .

--- NapolniRD/Program.cs
+++ NapolniRD/Program.cs
@@ -4,27 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NapolniRD;
+using System.Timers;
 
 namespace NapolniRD
 {
     class Program
     {
+        private static System.Timers.Timer aTimer;
         static void Main(string[] args)
+        {
+
+
+            aTimer = new System.Timers.Timer(10000);
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 10000;
+            aTimer.Enabled = true;
+
+            Console.ReadLine();
+        }
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
         {
             RDEntities db = new RDEntities();
             Random r = new Random();
-
-            for (int k=0; k<10; k++)  //leto
+            for (int k = 0; k < 1; k++)  //leto
             {
                 TestDatas nov = new TestDatas();
-                nov.Leto = k + 2018;
-                for(int l=1;l<13; l++)   //mesec
+                nov.Leto = k + 2028;
+                for (int l = 1; l < 13; l++)   //mesec
                 {
                     nov.Mesec = l;
-                    for(int j=1;j<31;j++)   //dan
+                    for (int j = 1; j < 31; j++)   //dan
                     {
                         nov.Dan = j;
-                        for(int p=1;p<6;p++)   //postaje
+                        for (int p = 1; p < 6; p++)   //postaje
                         {
                             nov.IdPostaje = p;
                             nov.Podatek1 = r.Next(5, 20) + r.Next(5, 20) + r.Next(5, 20);
@@ -34,11 +47,9 @@ namespace NapolniRD
                             Console.WriteLine(nov.IdPostaje + " " + nov.Dan + " " + nov.Mesec + " " + nov.Leto);
                         }
                     }
-                    
+
                 }
             }
-            db.SaveChanges();
-            Console.ReadLine();
         }
     }
 }
